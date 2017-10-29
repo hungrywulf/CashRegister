b984ebd828b35b09fd6e3768bad0904d964e3e15
hungrywulf <hungrywulf666@gmail.com>
2017-10-28 23:54:00 -0500

    More various improvements in the program's code

--- CashRegister.cs
+++ CashRegister.cs
@@ -11,59 +11,57 @@ class CashRegister {
         string input;
         double totalCost = 0; // amount customer cwes
 
-        List<string> alcoholItems = csvToArrayS(0, "alcohol.csv");
-        List<double> alcoholPrice = csvToArrayD(1, "alcohol.csv");
-        List<int> alcoholStock = csvToArrayI(2, "alcohol.csv");
+        List<string> alcoholItems = CSVListS(0, "alcohol.csv");
+        List<double> alcoholPrice = CSVListD(1, "alcohol.csv");
+        List<int> alcoholStock = CSVListI(2, "alcohol.csv");
 
-        List<string> beveragesItems = csvToArrayS(0, "beverages.csv");
-        List<double> beveragesPrice = csvToArrayD(1, "beverages.csv");
-        List<int> beveragesStock = csvToArrayI(2, "beverages.csv");
+        List<string> beveragesItems = CSVListS(0, "beverages.csv");
+        List<double> beveragesPrice = CSVListD(1, "beverages.csv");
+        List<int> beveragesStock = CSVListI(2, "beverages.csv");
 
-        List<string> foodItems = csvToArrayS(0, "food.csv");
-        List<double> foodPrice = csvToArrayD(1, "food.csv");
-        List<int> foodStock = csvToArrayI(2, "food.csv");
+        List<string> foodItems = CSVListS(0, "food.csv");
+        List<double> foodPrice = CSVListD(1, "food.csv");
+        List<int> foodStock = CSVListI(2, "food.csv");
 
-        List<string> snacksItems = csvToArrayS(0, "snacks.csv");
-        List<double> snacksPrice = csvToArrayD(1, "snacks.csv");
-        List<int> snacksStock = csvToArrayI(2, "snacks.csv");
+        List<string> snacksItems = CSVListS(0, "snacks.csv");
+        List<double> snacksPrice = CSVListD(1, "snacks.csv");
+        List<int> snacksStock = CSVListI(2, "snacks.csv");
 
 
         //prompt user for input 1-4
         Console.WriteLine("Enter 1-4 for the types of items to purchase: ");
         Console.WriteLine("Aftwards enter the number of the item and the amount of items you want to buy\n");  
         while (finished == false) {
-            Console.WriteLine("1) Alcohol");
-            Console.WriteLine("2) Beverages");
-            Console.WriteLine("3) Food");
-            Console.WriteLine("4) Snacks");
-            Console.WriteLine("5) Exit and Checkout");
+            Console.WriteLine("1) Alcohol\n2) Beverages\n3) Food\n4) Snacks\n5) Exit and Checkout");
             input = Console.ReadLine();
 
             if (input == "1") {
                 
-                catalog(ref totalCost, ref alcoholItems, ref alcoholPrice, ref alcoholStock);
+                Catalog(ref totalCost, ref alcoholItems, ref alcoholPrice, ref alcoholStock);
             }
 
             if (input == "2") {
 
-                catalog(ref totalCost, ref beveragesItems, ref beveragesPrice, ref beveragesStock);
+                Catalog(ref totalCost, ref beveragesItems, ref beveragesPrice, ref beveragesStock);
             }
 
             if (input == "3") {
 
-                catalog(ref totalCost, ref foodItems, ref foodPrice, ref foodStock);
+                Catalog(ref totalCost, ref foodItems, ref foodPrice, ref foodStock);
             }
 
             if (input == "4") {
 
-                catalog(ref totalCost, ref snacksItems, ref snacksPrice, ref snacksStock);
+                Catalog(ref totalCost, ref snacksItems, ref snacksPrice, ref snacksStock);
             }
             if (input == "5") {
-                Console.WriteLine("The total Cost is: {0}", totalCost);
+
+                Console.WriteLine("\nThe total Cost is: {0}\n", totalCost);
                 finished = true;
             }
             if (input != "1" && input != "2" && input != "3" && input != "4" && input != "5") {
-                Console.WriteLine("\nError incorrect input\n");
+                
+                Console.WriteLine("\nError: enter an integer 1 - 5.\n");
             }
         }
     }
@@ -78,7 +76,7 @@ class CashRegister {
         return amountOfItems * price;
     }
 
-    public static List<int> csvToArrayI (int column, string fileLocation) {
+    public static List<int> CSVListI (int column, string fileLocation) {
         string line;
         int parsing;
         List<int> items = new List<int>();
@@ -95,7 +93,7 @@ class CashRegister {
         }
     }
 
-    public static List<double> csvToArrayD (int column, string fileLocation) {
+    public static List<double> CSVListD (int column, string fileLocation) {
         string line;
         double parsing;
         List<double> items = new List<double>();
@@ -112,7 +110,7 @@ class CashRegister {
         }
     }
 
-    public static List<string> csvToArrayS (int column, string fileLocation) {
+    public static List<string> CSVListS (int column, string fileLocation) {
         string line;
         List<string> items = new List<string>();
 
@@ -126,13 +124,17 @@ class CashRegister {
             return items;
         }
     }
-
-    public static void catalog(ref double cost,
+    // references 3 lists and a cost value
+    public static void Catalog(ref double cost,
                                ref List<string> items,
                                ref List<double> price,
                                ref List<int> stock) {
+        bool success1 = true;
+        bool success2 = true;
         int count = 1;
-        string input2;
+        int item;
+        int amountOfItems;
+        string input;
         string [] option = new string[2];
 
         foreach (var i in items) {
@@ -140,26 +142,37 @@ class CashRegister {
             count++;
         }
         Console.WriteLine();
-        input2 = Console.ReadLine();
-        option = input2.Split(' ');
+        input = Console.ReadLine();
+
+        // option[0] is the item
+        // option[1] is the amount of items bought
+        option = input.Split(' ');
+
+        if (option.Length == 2) {
+            success1 = Int32.TryParse(option[0], out item);
+            success2 = Int32.TryParse(option[1], out amountOfItems);
+        } else if (option.Length == 1) {
+            success1 = Int32.TryParse(option[0], out item);
+            amountOfItems = 1;
+        } else {
+            Console.WriteLine("\nError: Enter 1 or 2 integers\n");
+            return;
+        }
+
+        if (success1 == false || success2 == false) {
+            Console.WriteLine("\nError: Enter 1 or 2 integers\n");
+            return;
+        }
 
         for (count = 1; count <= items.Count(); count++) {
 
-            if (option[0] == count.ToString()) {
+            if (item == count) {
 
-                if (Int32.Parse(option[0]) == count) {
-                                
-                    if (option.Length == 2) {
-                        int amountOfItems;
-                        Int32.TryParse(option[1], out amountOfItems);
-                        cost += Cost(amountOfItems, price[count - 1]);
-                        stock[Int32.Parse(option[0]) - 1] -= amountOfItems;
-                    } else if (option.Length == 1) {
-                        cost += Cost(price[count - 1]);
-                        stock[Int32.Parse(option[0]) - 1]--;
-                    } else {
-                        Console.WriteLine("\nError in input is too long or short\n");
-                    }
+                cost += Cost(amountOfItems, price[count - 1]);
+                stock[item - 1] -= amountOfItems;
+
+                if (stock[item - 1] <= 0) {
+                    stock[item - 1] = 0;
                 }
             }
         }
